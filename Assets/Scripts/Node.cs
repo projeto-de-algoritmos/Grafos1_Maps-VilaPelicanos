bce599dedab8e0f8497df73dcc6c3b8c3b8e3bb9
Giovanni <larissagomes2silva@gmail.com>
2023-09-17 16:09:14 -0300

    add: Criando classe dos Nodes do grafo
    Co-Authored-By: Giovanni <63993401+giovanni1106@users.noreply.github.com>

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField]
+    private int id;
+
+    [SerializeField]
+    private List<Node> nodesAdj = new();
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int _id)
+    {
+        id = _id;
+    }
+
+    public List<Node> getNodesAdj()
+    {
+        return nodesAdj;
+    }
+
+    public void setNodeAdj(Node node)
+    {
+        nodesAdj.Add(node);
+    }
+
+    public bool isAdjacente(Node node)
+    {
+        return nodesAdj.Contains(node) && node != this;
+    }
+}
